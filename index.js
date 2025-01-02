797d6eb4a9ba74c7fc8d757a4808e51a93f83ed8
jeevan-budha <jeevan.7budha@gmail.com>
2025-01-02 09:18:05 +0545

    Basic change in JS

--- index.js
+++ index.js
@@ -11,13 +11,9 @@ const showTime=()=>{
     hr=hr<10 ?`0${hr}`: hr;
     min=min<10?`0${min}`:min;
     sec=sec<10?`0${sec}`:sec;
-    hrs=hr-12;
 
-    if(hr>12){
-        time.innerHTML=`${hrs} : ${min} :${sec}`;
-    }else{
-        time.innerHTML=`${hrs} : ${min} :${sec}`
-    }
+    time.innerHTML=`${hr} : ${min} :${sec}`;
+    
     timeFormat.innerHTML= hr>12 ?"PM"  : "AM";
 }
 
